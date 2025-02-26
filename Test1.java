0d55a577e6d4b29b05284ca018b89a9c253221a9
Jalil <yusufjamil75@gmail.com>
2025-02-26 08:57:25 +0530

    26 Feb 857

--- Test1.java
+++ Test1.java
@@ -9,6 +9,10 @@ public class Test1
 	{
 		System.out.println("test 2");
 	}
+	public void test3() 
+	{
+		System.out.println("test 3");
+	}
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
